0c88131149e3c6f915796bc8435aa73ea60ee877
Ilhom77 <103019100+Ilhom77@users.noreply.github.com>
2022-05-03 17:32:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,116 +1,90 @@
 // Copyright 2022 NNTU-CS
 #include <string>
 #include <map>
-#include <stack>
 #include "tstack.h"
-int prior(char ee) {
-  switch (ee) {
-    case '(':
-      return 0;
-    case ')':
-      return 1;
-    case '+':
-      return 2;
-    case '-':
-      return 2;
-    case '*':
-      return 3;
-    case '/':
-      return 3;
-  }
-  return 0;
+int prior(char c) {
+  if (c == '(')
+    return 0;
+  else if ( c == ')')
+    return 1;
+  else if (c == '+' || c == '-')
+    return 2;
+  else if ( c == '*' || c == '/')
+    return 3;
+  else if (c == ' ')
+    return 4;
+  else
+    return 5;
 }
-bool search(std::string s, char ee) {
-  for (int i = 0; i < s.length(); ++i) {
-    if (s[i] == ee) {
-      return true;
-    }
-  }
-  return false;
-}
-
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
-  TStack<char, 100> stack;
-  std::stack<char> Stack;
-  std::string number = "0123456789";
-  std::string operation = "()+-*/";
-  std::string rslt = "";
-  for (int i = 0; i < inf.length(); ++i) {
-    if (search(number, inf[i])) {
-      rslt += inf[i];
-      rslt += ' ';
-    } else if (search(operation, inf[i])) {
-      if (prior(inf[i]) == 0) {
-        Stack.push(inf[i]);
-      } else if (Stack.empty()) {
-        Stack.push(inf[i]);
-      } else if (prior(inf[i]) > prior(Stack.top())) {
-        Stack.push(inf[i]);
-      } else if (inf[i] == ')') {
-        while (Stack.top() != '(') {
-          rslt += Stack.top();
-          rslt += ' ';
-          Stack.pop();
-        }
-        if (Stack.top() == '(') {
-          Stack.pop();
-        }
-      } else {
-        for (int j = 0; j<= Stack.size(); ++j) {
-          if (prior(Stack.top()) >= prior(inf[i])) {
-            rslt += Stack.top();
-            rslt += ' ';
-            Stack.pop();
-          }
-        }
-        Stack.push(inf[i]);
+  std::string outp;
+  TStack <char, 100> ts;
+  const char pr = ' ';
+  for (int i = 0; i < inf.length(); i++) {
+    if (prior(inf[i]) == 5) {
+      outp += inf[i];
+      outp += pr;
+    } else if (ts.isEmpty() || prior(inf[i]) == 0
+                     || (prior(inf[i]) > prior(ts.get()))) {
+      ts.push(inf[i]);
+    } else if (prior(inf[i]) == 1) {
+      while (prior(ts.get()) != 0) {
+        outp += ts.get();
+        outp += pr;
+        ts.pop();
       }
+      ts.pop();
+    } else if (prior(inf[i]) <= prior(ts.get())) {
+      while (prior(ts.get()) > 1 && !ts.isEmpty()) {
+        outp += ts.get();
+        outp += pr;
+        ts.pop();
+      }
+      ts.push(inf[i]);
     }
   }
-  if (!Stack.empty()) {
-    for (int j = 0; j <= Stack.size(); ++j) {
-      rslt += Stack.top();
-      rslt += ' ';
-      Stack.pop();
-    }
+while (!ts.isEmpty()) {
+  outp += ts.get();
+  outp += pr;
+  ts.pop();
   }
-  if (rslt[rslt.length() - 1] == ' ') {
-    rslt.erase(rslt.length() - 1);
-  }
-  return rslt;
+    outp.pop_back();
+  return outp;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
-  int sm = 0;
-  std::stack<int> Stack;
-  TStack<int, 100> stack;
-  std::string number = "0123456789";
-  std::string operation = "()+-*/";
-  for (int i = 0; i < pref.length(); ++i) {
-    if (search(number, pref[i])) 
-    
-      int a = 0, b = 0;
-      a = Stack.top();
-      Stack.pop();
-      b = Stack.top();
-      Stack.pop();
-      if (pref[i] == '+') {
-        Stack.push(b + a);
-      }
-      if (pref[i] == '-') {
-        Stack.push(b - a);
-      }
-      if (pref[i] == '*') {
-        Stack.push(b * a);
-      }
-      if (pref[i] == '/') {
-        Stack.push(b / a);
+  std::string outp;
+  TStack<int, 100> ts2;
+  int res;
+  for (int i = 0; i < pref.length(); i++) {
+    std::string ch;
+    while (5 == prior(pref[i])) {
+      ch += pref[i];
+      i += 1;
+    }
+    if (ch != "") {
+      ts2.push(std::stoi(ch));
+    }
+    if (2 == prior(pref[i]) || 3 == prior(pref[i])) {
+      int a = ts2.get();
+      ts2.pop();
+      int b = ts2.get();
+      ts2.pop();
+      if ('-' == pref[i]) {
+        res = b - a;
+        ts2.push(res);
+      } else if ('+' == pref[i]) {
+        res = a + b;
+        ts2.push(res);
+      } else if ('*' == pref[i]) {
+        res = a * b;
+        ts2.push(res);
+      } else if ('/' == pref[i]) {
+        res = b / a;
+        ts2.push(res);
       }
     }
   }
-  sm = Stack.top();
-  return sm;c
+  res = ts2.get();
+  return res;
+}
