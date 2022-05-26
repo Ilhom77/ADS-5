02370da33579cc4073ec080517f73c1f7882328d
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-26 13:03:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,80 +1,32 @@
 // Copyright 2021 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
-int priority(char alt) {
-    switch (alt) {
-    case '(': return 0;
-    case ')': return 1;
-    case '-': case '+': return 2;
-    case '*': case '/': return 3;
-    default: return 4;
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+
+template<typename T, int size>
+class TStack {
+ private:
+    T arr[100];
+    int top;
+ public:
+    TStack() :top(-1) { }
+    T get() const {
+        return arr[top];
     }
-}
-int count(int i, int j, char sum) {
-    switch (sum) {
-    case '-': return i - j;
-    case '+': return i + j;
-    case '*': return i * j;
-    case '/': return i / j;
-    default: return 0;
+    bool isEmpty() const {
+        return top == -1;
     }
-}
-std::string infx2pstfx(std::string inf) {
-  TStack <char, 100> stack;
-    int vari;
-    std::string str;
-  for (int i = 0; i < inf.size(); i++) {
-        vari = priority(inf[i]);
-        if (vari == 4) {
-            str.push_back(inf[i]);
-            str.push_back(' ');
-        } else {
-            if (((vari == 0) || stack.isEmpty())) {
-                stack.push(inf[i]);
-            } else if ((vari > priority(stack.get()))) {
-                stack.push(inf[i]);
-            } else if (vari == 1) {
-                while (stack.get() != '(') {
-                    str.push_back(stack.get());
-                    str.push_back(' ');
-                    stack.pop();
-                }
-                stack.pop();
-            } else if (inf[i] != ' ') {
-                int vrp = priority(stack.get());
-                while ((vrp >= priority(inf[i])) && (!stack.isEmpty())) {
-                    str.push_back(stack.get());
-                    str.push_back(' ');
-                    stack.pop();
-                }
-                stack.push(inf[i]);
-            }
-        }
+    bool isFull() const {
+        return top == size - 1;
     }
-  while (!stack.isEmpty()) {
-        str.push_back(stack.get());
-        str.push_back(' ');
-        stack.pop();
+    void pop() {
+        if (top >= 0)
+            top--;
     }
-    if (str[str.size() - 1] == ' ') {
-        str.erase(str.size() - 1);
+    void push(T item) {
+        if (top < size - 1)
+            arr[++top] = item;
     }
-    return str;
-}
-int eval(std::string prior) {
-  TStack <int, 100> stack;
-  int x, y;
-  for (int i = 0; i < prior.size(); i++) {
-      if (priority(prior[i]) < 4) {
-          y = stack.get();
-          stack.pop();
-          x = stack.get();
-          stack.pop();
-          stack.push(count(x, y, prior[i]));
-      } else if (prior[i] != ' ' && priority(prior[i]) == 4) {
-          stack.push(prior[i] - '0');
-      }
-  }
-  return stack.get();
-}
+};
+#endif  // INCLUDE_TSTACK_H_
+TStack<char, 100> stackA;
+TStack<int, 100> stackB;
