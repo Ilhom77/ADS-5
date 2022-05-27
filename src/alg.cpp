b0b3faa5ba235ee108bab7603c533cb493e3c422
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-27 19:11:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,32 +1,101 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
-class TStack {
- private:
-    T arr[100];
-    int top;
- public:
-    TStack() :top(-1) { }
-    T get() const {
-        return arr[top];
+// Copyright 2022 NNTU-CS
+#include <string>
+#include <map>
+#include "tstack.h"
+int prior(char znak) {
+  switch (znak) {
+    case '(':return 0;
+    case ')':return 1;
+    case '+':return 2;
+    case '-':return 2;
+    case '*':return 3;
+    case '/':return 3;
+    case ' ':return -2;
+    default:return -1;
+  }
+}
+void part(char znak, std::string* str) {
+  *str += znak;
+  *str += ' ';
+}
+int Calc(int a1, int a2, char symb) {
+  switch (symb) {
+    case '+':
+      return a2 + a1;
+    case '-':
+      return a2 - a1;
+    case '*':
+      return a2 * a1;
+    case '/':
+      return a2 / a1;
+    default:
+      throw "Error";
+  }
+}
+std::string infx2pstfx(std::string inf) {
+  return std::string("");
+  TStack<char, 128> stck;
+  std::string res = "";
+  for (int i = 0; i < inf.length(); i++) {
+    if (prior(inf[i]) == -1) {
+      if (i < inf.length() && prior(inf[i + 1]) == -1) {
+        while (i < inf.length() && prior(inf[i]) == -1) {
+          res += inf[i];
+          i++;
+        }
+        res += ' ';
+      } else {
+        part(inf[i], &res);
+      }
+      continue;
     }
-    bool isEmpty() const {
-        return top == -1;
+    if (stck.isEmpty() || prior(inf[i]) == 0
+        || prior(inf[i]) > prior(stck.get())) {
+      stck.push(inf[i]);
+    } else {
+      if (prior(inf[i]) == 1) {
+        while (prior(stck.get()) != 0) {
+          part(stck.get(), &res);
+          stck.pop();
+        }
+        stck.pop();
+      } else if (prior(inf[i]) <= prior(stck.get())) {
+        while (prior(stck.get()) > 1) {
+          part(stck.get(), &res);
+          stck.pop();
+        }
+        stck.push(inf[i]);
+      }
     }
-    bool isFull() const {
-        return top == size - 1;
+  }
+  while (!stck.isEmpty()) {
+    if (prior(stck.get()) > 1) {
+      part(stck.get(), &res);
     }
-    void pop() {
-        if (top >= 0)
-            top--;
+    stck.pop();
+  }
+  res.pop_back();
+  return res;
+}
+int eval(std::string post) {
+  TStack<int, 100> stSum;
+  for (int i = 0; i < post.length(); i++) {
+    int reslt = i;
+    std::string postfix = "";
+    while (prior(post[reslt]) == -1) {
+      postfix += post[reslt];
+      reslt++;
     }
-    void push(T item) {
-        if (top < size - 1)
-            arr[++top] = item;
+    i = reslt;
+    if (prior(post[i]) > 1) {
+      int a1 = stSum.get();
+      stSum.pop();
+      int a2 = stSum.get();
+      stSum.pop();
+      stSum.push(Calc(a1, a2, post[i]));
     }
-};
-#endif  // INCLUDE_TSTACK_H_
-TStack<char, 100> stackA;
-TStack<int, 100> stackB;
+    if (postfix != "") {
+      stSum.push(std::stoi(postfix));
+    }
+  }
+  return stSum.get();
