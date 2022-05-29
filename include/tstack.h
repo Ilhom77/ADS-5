b71d33a160115d165902123e0c1bea0e39c05320
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-29 11:00:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,1 +1,35 @@
-111
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+
+template<typename T, int size>
+class TStack {
+ private:
+  T arr[100];
+  int top;
+
+ public:
+  TStack() : top(-1) { }
+  bool isEmpty() const {
+    return top == -1;
+  }
+  bool isFull() const {
+    return top == size - 1;
+  }
+  void push(T value) {
+  if (isFull())
+    throw "Full!";
+  else
+    arr[++top] = value;
+  }
+  T get() const {
+    return arr[top];
+  }
+  void pop() {
+    if (!isEmpty())
+      --top;
+    else
+      throw "Empty!";
+  }
+};
+#endif  // INCLUDE_TSTACK_H_
