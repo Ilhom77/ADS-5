05dab5f16323487729675c79b98a0c734bdfc1e1
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-29 10:46:01 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,25 +1,32 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
- private:
-    T arr[100];
-    int top;
- public:
-    TStack() : top(-1) { }
-    T get() const {
-      return arr[top];
-    }
-    bool isEmpty() const {
-      return top == -1;
-    }
-    void pop() {
-      if (top >= 0) top--;
-    }
-    void push(T item) {
-      if (top < size - 1) arr[++top] = item;
-    }
+private:
+T arr[100];
+int top;
+
+public:
+TStack() :top(-1) { }
+T get() const {
+return arr[top];
+}
+bool isEmpty() const {
+return top == -1;
+}
+bool isFull() const {
+return top == size - 1;
+}
+void pop() {
+if (top >= 0)
+top--;
+}
+void push(T item) {
+if (top < size - 1)
+arr[++top] = item;
+}
 };
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
+TStack<char, 100> stackA;
+TStack<int, 100> stackB;
